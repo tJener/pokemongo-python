d94a4fc97324e6b51f77434c3b0d556760b4c834
Eric Li <li.zhe.hua@gmail.com>
2016-07-25 09:34:56 -0400

    Add command-line argument for named configs

--- example.py
+++ example.py
@@ -1,12 +1,32 @@
+from __future__ import print_function
 import sys
+import os.path
 import time
+import argparse
 import ConfigParser
 
 from skiplagged import Skiplagged
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', action='store', nargs=1, default='default', required=False)
+    options = parser.parse_args()
+
+    if isinstance(options.config, basestring):
+        config_path = 'config/%s.cfg' % options.config
+    else:
+        config_path = 'config/%s.cfg' % options.config[0]
+
+    if not os.path.isfile(config_path):
+        default_config = 'config/default.cfg'
+        eprint(config_path, ': file not found, defaulting to ', default_config)
+        config_path = default_config
+
     config = ConfigParser.RawConfigParser()
-    config_path = 'config/default.cfg'
+    print('Using config', config_path)
     config.read(config_path)
 
     client = Skiplagged()
@@ -24,27 +44,31 @@ if __name__ == '__main__':
             # Log in with a Google or Pokemon Trainer Club account
             method = config.get('pogo', 'login_method')
             if method == 'ptc':
-                print client.login_with_pokemon_trainer(
-                    config.get('pogo', 'ptc_username'),
-                    config.get('pogo', 'ptc_password')
+                print(
+                    client.login_with_pokemon_trainer(
+                        config.get('pogo', 'ptc_username'),
+                        config.get('pogo', 'ptc_password')
+                    )
                 )
             elif method == 'google':
-                print client.login_with_google(
-                    config.get('pogo', 'google_username'),
-                    config.get('pogo', 'google_password')
+                print(
+                    client.login_with_google(
+                        config.get('pogo', 'google_username'),
+                        config.get('pogo', 'google_password')
+                    )
                 )
             else:
                 sys.exit('Unrecognized login_method "%s" in %s' % (method, config_path))
 
             # Get specific Pokemon Go API endpoint
-            print client.get_specific_api_endpoint()
+            print(client.get_specific_api_endpoint())
 
             # Get profile
-            print client.get_profile()
+            print(client.get_profile())
 
             # Find pokemon
             for pokemon in client.find_pokemon(bounds):
-                print pokemon
+                print(pokemon)
         except Exception as e:
             print("Exception: {0} {1}".format(e.message, e.args))
             time.sleep(1)
